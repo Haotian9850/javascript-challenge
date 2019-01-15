50548306b2cbaaca29cc01f421f185964c3f50e6
Haotian Liu <haotian@pop-os.localdomain>
2019-01-14 20:43:05 -0500

    flex panel finished

--- flex-panels/script.js
+++ flex-panels/script.js
@@ -1,3 +1,5 @@
+//toggle: change class names
+
 
 const panels = document.querySelectorAll('.panel');    //returns a node list
 
@@ -5,4 +7,13 @@ function toggleOpen(){
     this.classList.toggle('open');
 }
 
+function toggleActive(e){
+    console.log(e.propertyName);    //echoes out what is being transitioned
+    if(e.propertyName.includes('flex')){
+        this.classList.toggle('open-active');
+    }
+    this.classList.toggle('open-active');
+}
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));   //toggle action
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive)); 
