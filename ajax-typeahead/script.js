44eca63b486ee1cb13ff60a66b9f0392d2137acb
Haotian Liu <haotian@pop-os.localdomain>
2019-01-15 17:28:14 -0500

    ajax typehead building

--- ajax-typeahead/script.js
+++ ajax-typeahead/script.js
@@ -1,5 +1,35 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const cities = []; //city array
+var cities = []; //city array
 
-fetch(endpoint).then(blob => console.log(blob));    //fetch: return a promise
+//fetch: return a promise. overall returns data array
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data));    //spread into array. put all 1000 cities into array cities 
+
+console.log(cities);
+
+function findMatch(wordToMatch, cities){
+    return cities.filter(place => {
+        //if city and state matches wordToMatch
+        //cant use regex here...
+        //create regex outisde...
+        const regex = new RegExp(wordToMatch, 'gi');    //global (g) and insensitive (i)
+        return place.city.match(regex) || place.state.match(regex); //match in both cities and state
+    });
+}
+
+function displayMatches(){
+    console.log(this.value);    //for testing
+    //get search phrase
+    
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+
+
+
+
