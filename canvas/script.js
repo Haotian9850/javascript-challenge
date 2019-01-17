75df189c718062f6a3e8643def3ebe9ac77d304b
Haotian Liu <haotian@pop-os.localdomain>
2019-01-17 17:30:06 -0500

    canvas working

--- canvas/script.js
+++ canvas/script.js
@@ -16,14 +16,30 @@ let lastX = 0;
 let lastY = 0;  //last X / Y
 
 function draw(e){
-    console.log(e);
+
     if(!isDrawing){
         return;
     }
+    console.log(e); //only executing while mouse is held down and moving
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    /*
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+    */
+    //es6 trick
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    
 }
 
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    //prevent new line from starting from where the last line ends
+    [lastX, lastY] = e.offsetX, e.offsetY;
+});
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);   //so that only holding the mouse will trigger event
 
