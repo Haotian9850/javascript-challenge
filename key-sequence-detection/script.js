5fff0e198212c3b6209f886fb209fdeda718d763
haotian <haotian@pop-os.localdomain>
2019-01-21 16:16:21 -0500

    key sequence detection finished

--- key-sequence-detection/script.js
+++ key-sequence-detection/script.js
@@ -1,3 +1,20 @@
 window.addEventListener('keyup', (e) => {
     console.log(e.key);
+    pressed.push(e.key);
+    console.log(pressed);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length); //get last len letters of array pressed
+    console.log(pressed);
+
+    //check
+    if(pressed.join('').includes(secretCode)){   //convert to string
+        console.log('Secret code detected!');
+    }
 }); //will log key anywhere within current window
+
+const pressed = [];
+const secretCode = 'haotian';   //sequence that needs to be detected
+
+//trim array to length of secret code
+
+
+
