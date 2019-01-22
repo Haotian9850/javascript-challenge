1170d75eae8dd88a7acdb9dd2016982321cf523d
haotian <haotian@pop-os.localdomain>
2019-01-22 17:14:53 -0500

    fly in image finished

--- slide-in-scroll/script.js
+++ slide-in-scroll/script.js
@@ -1,3 +1,4 @@
+//ensure that checkSlides only runs once every 20 miliseconds
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function() {
@@ -11,5 +12,29 @@ function debounce(func, wait = 20, immediate = true) {
       timeout = setTimeout(later, wait);
       if (callNow) func.apply(context, args);
     };
-  } //funtion that returns another function???
-  
+  }; //funtion that returns another function???
+
+//select all images
+const sliderImages = document.querySelectorAll('.slide-in');
+
+//check slide action
+function checkSlide(e){
+    console.log(window.scrollY);
+    //loop over every single image and determine their position
+    sliderImages.forEach(sliderImage => {
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;   //when every image is half way seen
+        console.log(slideInAt);
+        //image bottom
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if(isHalfShown && isNotScrolledPast){
+            //get image
+            sliderImage.classList.add('active');
+        }else{
+            sliderImage.classList.remove('active'); //in case the user scrolls back up...
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
